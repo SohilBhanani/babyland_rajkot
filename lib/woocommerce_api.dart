351bd88116ed492d41c7211bf164ecdc3f793a0c
SohilBhanani <sbhananis@gmail.com>
2021-05-01 20:12:09 +0530

    API Fix

--- lib/woocommerce_api.dart
+++ lib/woocommerce_api.dart
@@ -1,13 +1,11 @@
 import 'package:woocommerce/woocommerce.dart';
+import 'config.dart';
 
+List ConfigAPI = liveAPI;
 WooCommerce woo = WooCommerce(
-  // baseUrl: "https://babyland123.000webhostapp.com",
-  // consumerKey: "ck_8b3bbc8d84793131a04976e59aec5a34253834ea",
-  // consumerSecret: "cs_73f214ec3a64419286453f98ebf815561f3c2fff",
 
-  // `bluehost`
-
-  baseUrl: "https://babylandrajkot.com",
-  consumerKey: "ck_1fb1c5b82fbfdb5d338bdabde8daf18a5034c7b0",
-  consumerSecret: "cs_b11515dcd110cfbf7f9fb240f319e0e6c2f142e7",
+  //Enter your API Configurations HERE
+  baseUrl: ConfigAPI[0],
+  consumerKey: ConfigAPI[1],
+  consumerSecret: ConfigAPI[2]
 );
