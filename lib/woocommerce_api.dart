5d9e05abb412ff30ba30f4869d23f7a6a141546f
SohilBhanani <sbhananis@gmail.com>
2021-05-01 20:52:24 +0530

    API fix

--- lib/woocommerce_api.dart
+++ lib/woocommerce_api.dart
@@ -1,11 +1,10 @@
 import 'package:woocommerce/woocommerce.dart';
 import 'config.dart';
 
-List ConfigAPI = liveAPI;
+List configAPI = liveAPI;
 WooCommerce woo = WooCommerce(
-
   //Enter your API Configurations HERE
-  baseUrl: ConfigAPI[0],
-  consumerKey: ConfigAPI[1],
-  consumerSecret: ConfigAPI[2]
+  baseUrl: configAPI[0],
+  consumerKey: configAPI[1],
+  consumerSecret: configAPI[2]
 );
